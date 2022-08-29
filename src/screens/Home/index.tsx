1ea0af028f4581cef34035410a868314929cd702
Raquel <raquel.nobre@corebiz.ag>
2022-08-29 20:15:34 -0300

    styled components and touchableOpacity

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,30 @@
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
 export default function Home() {
+
+    function handleParticipantAdd(){
+    console.log('você clicou!');
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.eventName}>React</Text>
             <Text style={styles.eventDate}>Native</Text>
+            <TextInput 
+            style={styles.input}
+            placeholder='Nome do participante'
+            placeholderTextColor= "#6b6b6b"/>
+
+            <TouchableOpacity style={styles.button}
+            onPress={handleParticipantAdd}
+            >
+                <Text style={styles.buttonText}>
+                    +
+                </Text>
+            </TouchableOpacity>
+
+            
         </View>
     );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,6 +15,27 @@ export const styles = StyleSheet.create({
     eventDate: {
         color: '#6b6b6b',
         fontSize: 16
+    },
+    input: {
+        height: 56,
+        backgroundColor: '#1f1e25',
+        borderRadius: 5,
+        color: '#fff',
+        padding: 16,
+        fontSize: 16,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 24,
+        
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#31cf67',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 
 });
